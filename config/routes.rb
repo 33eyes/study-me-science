bd0cbdc61d8150dece4c0cd68664f2630ead1a5f
a33eyes <ariiigu@gmail.com>
2017-04-05 22:12:47 +0000

    Add an Under Construction page, and redirect links to pages under construction to it for now.

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,24 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get '/help', to: 'static_pages#help'
+#  get '/help', to: 'static_pages#help'
 
-  get '/about', to: 'static_pages#about'
+#  get '/about', to: 'static_pages#about'
   
-  get '/contact', to: 'static_pages#contact'
+#  get '/contact', to: 'static_pages#contact'
   
-  get '/signup', to: 'users#new'
+#  get '/signup', to: 'users#new'
+
+
+
+  get '/under_construction', to: 'static_pages#under_construction'
+  
+  # Under construction pages
+  get '/help', to: 'static_pages#under_construction'
+  get '/about', to: 'static_pages#under_construction'
+  get '/contact', to: 'static_pages#under_construction'
+  get '/signup', to: 'static_pages#under_construction'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,19 +10,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Help | StudyMe.Science"
+#    assert_select "title", "Help | StudyMe.Science"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | StudyMe.Science"
+#    assert_select "title", "About | StudyMe.Science"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | StudyMe.Science"
+#    assert_select "title", "Contact | StudyMe.Science"
+  end
+  
+  test "should get under-construction" do
+    get under_construction_path
+    assert_response :success
+    assert_select "title", "Page under construction | StudyMe.Science"
   end
   
 end
