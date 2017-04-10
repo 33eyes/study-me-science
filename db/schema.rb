d8aa3fa39ad638da2d2527b6a3c8bd1c55b9293d
a33eyes <ariiigu@gmail.com>
2017-04-10 03:02:33 +0000

    Create contacts table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408033249) do
+ActiveRecord::Schema.define(version: 20170410025851) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
